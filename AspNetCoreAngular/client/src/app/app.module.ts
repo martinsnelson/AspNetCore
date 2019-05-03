30ead4782d3c5a453e44968817459d661caecd6e
martinsnelson <nelsontecti@gmail.com>
2019-05-03 03:08:06 -0300

    Add Componente Login, Registrar, Produto

--- AspNetCoreAngular/client/src/app/app.module.ts
+++ AspNetCoreAngular/client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrarComponent } from './auth/registrar/registrar.component';
+import { ProdutoComponent } from './produto/produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrarComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule,
