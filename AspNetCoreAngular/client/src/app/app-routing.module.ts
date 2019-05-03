3199eefe28182c4dc9812d4e6331799655268e9e
martinsnelson <nelsontecti@gmail.com>
2019-05-03 03:17:39 -0300

    add Rotas login, registrar e produto

--- AspNetCoreAngular/client/src/app/app-routing.module.ts
+++ AspNetCoreAngular/client/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProdutoComponent } from './produto/produto.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrarComponent } from './auth/registrar/registrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'produto',
+    component: ProdutoComponent,
+    data: { title: 'Lista de Produtos' }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { title: 'Login' }
+  },
+  {
+    path: 'registrar',
+    component: RegistrarComponent,
+    data: { title: 'Registrar' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
